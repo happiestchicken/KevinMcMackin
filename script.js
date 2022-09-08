311d83ae8875fd007ae737c9c9e5a335bb498976
iamkevin <kevinmcmackin1@gmail.com>
2022-09-07 22:43:48 -0300

    update

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
 function CS50_appear() {    
-    document.getElementById("CS50-display").style.display="flex";
-    document.getElementById("CS50-display").style.flexDirection="row";
-    document.getElementById("python-display").style.display="none"
+    document.getElementById("cs50-container").style.display="grid";
+    document.getElementById("python-container").style.display="none"
+    document.getElementById("java-container").style.display="none";
+    document.getElementById("js-container").style.display="none";
 }
 
 function python_appear() {    
-    document.getElementById("python-display").style.display="flex";
-    document.getElementById("python-display").style.flexDirection="row";
-    document.getElementById("CS50-display").style.display="none";
-
+    document.getElementById("python-container").style.display="grid";
+    document.getElementById("cs50-container").style.display="none";
+    document.getElementById("java-container").style.display="none";
+    document.getElementById("js-container").style.display="none";
 }
 
-const sections = [...document.querySelectorAll("section")];
+function java_appear() {    
+  document.getElementById("java-container").style.display="grid";
+  document.getElementById("cs50-container").style.display="none";
+  document.getElementById("python-container").style.display="none";
+  document.getElementById("js-container").style.display="none";
+}
 
-let options = {
-  rootMargin: "0px",
-  threshold: 0.75,
-};
-
-const callback = (entries, observer) => {
-  entries.forEach((entry) => {
-    const { target } = entry;
-
-    if (entry.intersectionRatio >= 0.75) {
-      target.classList.add("is-visible");
-    } else {
-      target.classList.remove("is-visible");
-    }
-  });
-};
-
-const observer = new IntersectionObserver(callback, options);
-
-sections.forEach((section, index) => {
-  const sectionChildren = [...section.querySelector("[data-content]").children];
-
-  sectionChildren.forEach((el, index) => {
-    el.style.setProperty("--delay", `${index * 250}ms`);
-  });
-
-  observer.observe(section);
-});
+function js_appear() {
+  document.getElementById("js-container").style.display="grid";
+  document.getElementById("java-container").style.display="none";
+  document.getElementById("cs50-container").style.display="none";
+  document.getElementById("python-container").style.display="none";
+}
